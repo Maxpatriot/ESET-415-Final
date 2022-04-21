81f7ec7d481ae8abd654ca9bec6c26817e5d630f
Maximilian Deitrick <americana2013@gmail.com>
2022-04-21 16:55:00 -0500

    exchanges keys

--- client.py
+++ client.py
@@ -4,6 +4,9 @@ import helper as help
 _host = "127.0.0.1"
 _port = 8000
 
+p, q, e, d = 83, 61, 53, 557
+n = p * q
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.settimeout(10)
     try:
@@ -18,5 +21,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     message = input("Input a message to send\n-> ")
     s.sendall(message.encode())
     data = s.recv(1024)
-
-print("Received {}".format(data.decode()))
+    while data.decode() != "EXIT":
+        print("Received {}".format(data.decode()))
+        s.sendall("{},{}".format(e, n).encode())
--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 import socket
+import helper as help
 
 _host = "127.0.0.1"
 _port = 8000
 
+p, q, e, d = 47, 71, 97, 1693
+n = p * q
+
+client_e = 0
+client_n = 0
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.settimeout(15)
     s.bind((_host, _port))
     s.listen()
     conn, addr = s.accept()
@@ -11,6 +19,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print("Connected by {}".format(addr))
         while True:
             data = conn.recv(1024)
+            print(data.decode())
             if not data:
                 break
-            conn.sendall(data)
+            if data.decode() == "REGISTER":
+                print("hello")
+                conn.sendall(("keys,{},{}".format(e, n)).encode())
+                data = conn.recv(1024)
+                client_e, client_n = data.decode().split(',')
+                print(client_e)
+                print(client_n)
+
